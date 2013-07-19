34e99008d57a783794689a5d160d7c4f9a4ebc59
YuviPanda <yuvipanda@gmail.com>
2013-07-19 23:47:53 +0530

    Set proper name for jsub naming

--- src/run.bash
+++ src/run.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-jsub -name lolrrit-wm -mem 1G -continuous node /data/project/lolrrit-wm/lolrrit-wm/src/relay.js
+jsub -N lolrrit-wm -mem 1G -continuous node /data/project/lolrrit-wm/lolrrit-wm/src/relay.js
