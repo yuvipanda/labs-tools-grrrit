232873f4487fa5ec7e5309308a2cfb4816ebcccf
YuviPanda <yuvipanda@gmail.com>
2013-07-20 17:28:05 +0530

    Make run.bash kill, wait and then start.

--- src/run.bash
+++ src/run.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Stop current job if it exists, and start a new one
 jstop lolrrit-wm
+sleep 2s # Stupid hack to make sure the job is killed
 jsub -N lolrrit-wm -mem 1G -continuous node /data/project/lolrrit-wm/lolrrit-wm/src/relay.js
